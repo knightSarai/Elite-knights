d50206451d79a3f04d98b06031be2854890d52b9
knightSarai <knight.sarai.dev@gmail.com>
2020-05-19 19:31:33 +0300

    morgan

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
-
+const morgan = require("morgan");
 // Routs files
 const trainingcamps = require("./routs/trainingcamps");
 
@@ -8,6 +8,11 @@ const trainingcamps = require("./routs/trainingcamps");
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
+
+// Dev loggin middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 // Mount routers
 app.use("/api/v1/trainingcamps", trainingcamps);
