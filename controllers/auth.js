2164e0ad7bc7e65a1e517ad63f2cba6a81bf06d9
knightSarai <knight.sarai.dev@gmail.com>
2020-05-29 10:24:45 +0300

    update user details (name, email) and password

--- controllers/auth.js
+++ controllers/auth.js
@@ -115,6 +115,42 @@ exports.resetpassword = asyncHandler(async (req, res, next) => {
 	sendTokenResponse(user, 200, res);
 });
 
+// @desc   Update user details
+// @route  GET /api/v1/auth/updatedetails
+// @access private
+exports.updateDetails = asyncHandler(async (req, res, next) => {
+	const fieldsToUpdate = {
+		name: req.body.name,
+		email: req.body.email
+	};
+
+	const user = await User.findByIdAndUpdate(req.user.id, fieldsToUpdate, {
+		new: true,
+		runValidators: true
+	});
+	res.status(200).json({
+		success: true,
+		data: user
+	});
+});
+
+// @desc   Update password
+// @route  PUT /api/v1/auth/updatepassword
+// @access Private
+exports.updatePassword = asyncHandler(async (req, res, next) => {
+	const user = await User.findById(req.user.id).select('+password');
+
+	// Check current password
+	if (!await user.matchPassword(req.body.currentPassword)) {
+		return next(new ErrorResponse('Password is incorrect', 401));
+	}
+
+	user.password = req.body.newPassword;
+	await user.save();
+
+	sendTokenResponse(user, 200, res);
+});
+
 // Get token from model, create cookie and response
 const sendTokenResponse = (user, statusCode, res) => {
 	// Create token
--- routs/auth.js
+++ routs/auth.js
@@ -1,5 +1,13 @@
 const express = require('express');
-const { register, login, getMe, forgotPassword, resetpassword } = require('../controllers/auth');
+const {
+	register,
+	login,
+	getMe,
+	forgotPassword,
+	resetpassword,
+	updateDetails,
+	updatePassword
+} = require('../controllers/auth');
 
 const router = express.Router();
 
@@ -8,6 +16,8 @@ const { protect } = require('../middleware/auth');
 router.post('/register', register);
 router.post('/login', login);
 router.get('/me', protect, getMe);
+router.put('/updatedetails', protect, updateDetails);
 router.post('/forgotpassword', forgotPassword);
+router.put('/updatepassword', protect, updatePassword);
 router.put('/resetpassword/:resettoken', resetpassword);
 module.exports = router;
